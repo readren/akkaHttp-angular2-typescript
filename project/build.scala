632e123373149241a5056ef8cdb83bf5f147f02f
readren <gustavopollitzer@hotmail.com>
2016-05-12 06:54:09 -0300

    Correct baddly closed comment.

--- project/build.scala
+++ project/build.scala
@@ -9,8 +9,9 @@ import sbt._
 import sbt.Keys._
 import WebKeys._
 
-/**Nests the definition of project settings that are too complex to be put in the "build.sbt" file.
- * There is only one setting for now: the mirrorNodeModules task.  */
+/**
+ * Nests the definition of project settings that are too complex to be put in the "build.sbt" file.
+ * There is only one setting for now: the mirrorNodeModules task.
  */
 object projectAddOns {
   val mirrorNodeModules = TaskKey[File]("mirrorNodeModules", "Typescript modules resolution mechanism requires that the 'mode_modules' folder be a sibbling of a folder that contains all the typescript source files. And SBT requires that managed files be inside the 'target' folder. So, to comply with both we let SBT generate the 'node_modules' folder where he wants, and create a virtual folder that mirrors said folder in a place where the typescript's module resolution mechanisms can find it: inside the parent folder of the assets's sourceDirectoy. This task creates said virtual folder.")
@@ -27,9 +28,11 @@ object projectAddOns {
     virtualNodeModulesDirectory
   }
 
-  /**Creates a virtual directory that mirrors the contens of another directory. If the virtual directory already exists, nothings happens.
+  /**
+   * Creates a virtual directory that mirrors the contens of another directory. If the virtual directory already exists, nothings happens.
    * @param of a file indicating the real directory path, the one to be imitated
-   * @param at a file indicating the virtual directory path */
+   * @param at a file indicating the virtual directory path
+   */
   private def createVirtualDirectory(of: File, at: File, logger: Logger): Unit = {
     if (!at.exists) {
       try {
